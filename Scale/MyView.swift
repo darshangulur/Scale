d11f93b78ba9f91bc84a166c5a05fb4ec9411d0e
Darshan Gulur Srinivasa <Darshan.Srinivasa.contractor@hilton.com>
2020-07-24 07:52:16 -0500

    Created new view

---
+++ Scale/MyView.swift
@@ -0,0 +1,13 @@
+//
+//  MyView.swift
+//  Scale
+//
+//  Created by Darshan Srinivasa on 7/24/20.
+//  Copyright © 2020 com.poc.swift. All rights reserved.
+//
+
+import UIKit
+
+public class MyView: UIView {
+    
+}
