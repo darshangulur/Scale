38c0af3acf235f67b979458214ca861efba0cb65
Darshan Gulur Srinivasa <darshangulur@gmail.com>
2020-07-24 07:21:40 -0500

    Added project files

---
+++ Scale/Measure.swift
@@ -0,0 +1,15 @@
+//
+//  Measure.swift
+//  Scale
+//
+//  Created by Darshan Srinivasa on 7/24/20.
+//  Copyright © 2020 com.poc.swift. All rights reserved.
+//
+
+import Foundation
+
+public enum Measurement {
+    case foreArm
+    case shoulder
+    case collar
+}
---
+++ Scale/Scale.h
@@ -0,0 +1,19 @@
+//
+//  Scale.h
+//  Scale
+//
+//  Created by Darshan Srinivasa on 7/24/20.
+//  Copyright © 2020 com.poc.swift. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Scale.
+FOUNDATION_EXPORT double ScaleVersionNumber;
+
+//! Project version string for Scale.
+FOUNDATION_EXPORT const unsigned char ScaleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Scale/PublicHeader.h>
+
+
